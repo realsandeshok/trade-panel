9e31125b30c76d751d8b780901389ea900269b1f
Sandesh <sandeshmadoori@gmail.com>
2024-09-23 16:27:12 +0530

    sectorRoute

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const accountRoutes = require('./routes/accountRoutes');
 const scriptRoutes = require('./routes/scriptRoutes');
 const transactionRoutes = require('./routes/transactionsRoutes');
 const holdingsRoutes = require('./routes/holdingsRoutes')
-
+const sectorRoutes = require('./routes/sectorRoutes')
+ 
 const app = express();
 const port = 3000;
 
@@ -20,6 +21,9 @@ app.use('/scripts', scriptRoutes); // Add route for scripts
 // app.use('/transaction',transRoutes)
 app.use('/holdings', holdingsRoutes)
 app.use('/transactions', transactionRoutes)
+app.use('/sectors', sectorRoutes)
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
